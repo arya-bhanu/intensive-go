afbbb00d7ef29c9a812fa8104698d2174139a1e3
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-11-16 16:05:17 +0700

    init: medium, whole new knowledge about:
    FieldsFunc using delimiter function

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
 
 func main() {
 	fmt.Println("Hello World")
+}
+
+func CountWordFrequency(text string) map[string]int {
+	IsDelimiter := func(c rune) bool {
+		return c == ' ' || c == '\n' || c == '\t' || c == '-'
+	}
+
+	maps := make(map[string]int)
+	splitted := strings.FieldsFunc(text, IsDelimiter)
+
+	if len(splitted) == 1 && splitted[0] == "" {
+		return maps
+	}
+
+	for _, word := range splitted {
+		lowered := FilterLetter(strings.ToLower(word))
+
+		currCount, ok := maps[lowered]
+		if !ok {
+			maps[lowered] = 1
+			continue
+		}
+		currCount++
+		maps[lowered] = currCount
+	}
+	return maps
+}
+
+func FilterLetter(word string) string {
+	var cleanWord []rune
+	for _, r := range word {
+		if unicode.IsLetter(r) || unicode.IsNumber(r) {
+			cleanWord = append(cleanWord, r)
+		}
+	}
+	return string(cleanWord)
 }
