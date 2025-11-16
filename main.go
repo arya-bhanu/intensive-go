1227b828b51dc39cc9ca4a87022d237c6aca38f3
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-11-16 16:12:06 +0700

    feat: easy done

--- main.go
+++ main.go
@@ -5,3 +5,11 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World")
 }
+
+func ReverseString(str string) string {
+	var runes []rune
+	for i := len(str) - 1; i >= 0; i-- {
+		runes = append(runes, rune(str[i]))
+	}
+	return string(runes)
+}
