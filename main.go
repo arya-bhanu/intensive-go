7ac2c72db00d2af16255b0d8f506450b0a7b80c2
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-11-21 10:56:49 +0700

    feat: ez bangey

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	fmt.Println(IsPalindrome("A man, a plan, a canal: Panama"))
+}
+
+func IsPalindrome(s string) bool {
+	lowered := strings.ToLower(s)
+	cleanedString := ""
+	for _, r := range lowered {
+		if validString(r) {
+			cleanedString += string(r)
+		}
+	}
+	maxloop := len(cleanedString) / 2
+	for i := range maxloop {
+		j := len(cleanedString) - 1 - i
+		if cleanedString[i] != cleanedString[j] {
+			return false
+		}
+	}
+	return true
+}
+
+func validString(r rune) bool {
+	return unicode.IsLetter(r) || unicode.IsNumber(r)
 }
